313a75b7b893fdc5e6324c8bc4e580f03071a093
Heshamsayed103 <hesham58382004@gmail.com>
2025-08-30 14:13:12 +0300

    Demo

---
+++ Demo/Configration/EmployeeConfigration.cs
@@ -0,0 +1,26 @@
+﻿using EF_01.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_01.Configration
+{
+    internal class EmployeeConfigration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasKey(E => E.EmpId);
+
+            modelBuilder.Entity<Employee>().Property(E => E.Name).IsRequired().HasColumnType("varchar").HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>().Property(E => E.Salary).IsRequired(false);
+
+            modelBuilder.Entity<Employee>().Property(E => E.DateOfCreation).HasDefaultValue(DateTime.Now);
+        }
+    }
+}
---
+++ Demo/Contexts/AppDbContext.cs
@@ -0,0 +1,54 @@
+﻿using EF_01.Configration;
+using EF_01.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_01.Contexts
+{
+    internal class AppDbContext : DbContext
+    {
+
+        // Microsoft.EntityFrameworkCore
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Employee>().HasKey("EmpId");
+            //modelBuilder.Entity<Employee>().HasKey(E => E.EmpId);
+
+            //modelBuilder.Entity<Employee>().Property(E => E.Name).IsRequired().HasColumnType("varchar").HasMaxLength(50);
+
+            //modelBuilder.Entity<Employee>().Property(E => E.Salary).IsRequired(false);
+
+            //modelBuilder.Entity<Employee>().Property(E => E.DateOfCreation).HasDefaultValue(DateTime.Now);
+
+
+            modelBuilder.ApplyConfiguration(new EmployeeConfigration());
+
+
+        }
+
+
+
+        public AppDbContext() : base() 
+        {
+            
+        }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = App01; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
